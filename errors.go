f7531eff394f74852e2961b536263758a6a5d6f3
ColonelBlimp <20266202+ColonelBlimp@users.noreply.github.com>
2025-11-03 15:15:28 +0200

    Add validation for database configuration structs
    Integrated `validator` package to enforce required fields in `Config`. Updated `Config` struct with validation tags and enhanced error handling with detailed error utilities. Added tests to ensure proper validation and error propagation.

--- errors.go
+++ errors.go
@@ -1,5 +1,9 @@
 package errors
 
+import (
+	stderr "errors"
+)
+
 type Op string
 
 type Error interface {
@@ -22,13 +26,48 @@ func New(op Op) *DetailedError {
 	}
 }
 
+// AsDetailedError attempts to cast the given error to a DetailedError instance.
+func AsDetailedError(err error) (*DetailedError, bool) {
+	var dErr *DetailedError
+	if stderr.As(err, &dErr) {
+		return dErr, true
+	}
+	return nil, false
+}
+
 // Error implements the error interface.
 func (e *DetailedError) Error() string {
+	if e == nil {
+		return ""
+	}
 	return e.msg
 }
 
 // Msg sets the human-readable error message for a DetailedError instance.
-func (e *DetailedError) Msg(msg string) error {
+func (e *DetailedError) Msg(msg string) *DetailedError {
+	if e == nil {
+		return nil
+	}
 	e.msg = msg
 	return e
+}
+
+// Err sets the cause for a DetailedError instance.
+func (e *DetailedError) Err(err error) *DetailedError {
+	if e == nil {
+		return nil
+	}
+	e.cause = err
+	return e
+}
+
+// Cause returns the cause of a DetailedError instance.
+func (e *DetailedError) Cause() error {
+	if e == nil {
+		return nil
+	}
+	if e.cause == nil {
+		return nil
+	}
+	return e.cause
 }
