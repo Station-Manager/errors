2ea3cdfb03f47efc00914a9732d3676ac0ac03a5
ColonelBlimp <20266202+ColonelBlimp@users.noreply.github.com>
2025-11-03 08:34:57 +0200

    Add initial project setup with error handling package and IDE configuration

---
+++ errors.go
@@ -0,0 +1,17 @@
+package errors
+
+type Op string
+
+type Error interface {
+	error
+}
+
+type DetailedError struct {
+	Op    Op
+	Cause error
+	Msg   string // Human-readable error message
+}
+
+func (e *DetailedError) Error() string {
+	return e.Msg
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Station-Manager/errors
+
+go 1.25
